2cc8cb9a07865b9c7acbe39a54a01b03663c1c91
JefriAQ <82922297+JefriAQ@users.noreply.github.com>
2022-04-01 21:04:42 -0400

    Add files via upload
    php crud mysql de practica

---
+++ php_crud_mysql/db.php
@@ -0,0 +1,14 @@
+<?php
+    $conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'php_crud'
+);
+
+// if (isset($conn)){
+//     echo "Db is connceted";
+// }else{
+//     echo "Db disconnected";
+// }
+?>
